35f05ebc31dc32fa56e3ea21bc6177183ebecbc9
SapphireDakota <pairav3015@mail.ru>
2024-07-10 10:40:49 +0300

    Writing logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
 func main() {
 	// TODO: инициализировать объект конфигурации
 
@@ -8,4 +14,24 @@ func main() {
 	// TODO: инициализировать приложение (app)
 
 	// TODO: запустить gRPC-сервер приложения
+
+	config := config.MustLoad()
+	log := setupLogger(config.Env)
+
+	log.Info("starting application",
+		slog.Any("config", *config),
+	)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "prod":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+	return log
 }
