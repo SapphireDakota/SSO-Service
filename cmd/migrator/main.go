740656fc7a595c941fe03e4ddaed23f2a59ab0f1
Пайрав Бузургхонов <buzurgkhonov@trueconf.ru>
2025-10-15 11:31:56 +0300

    Добавил пакет `storage`, а также мигратор и миграции БД

---
+++ cmd/migrator/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+
+	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	var (
+		storagePath, migrationsPath, migrationsTable string
+	)
+
+	flag.StringVar(&storagePath, "storage-path", "storage", "The path where the storage is stored.")
+	flag.StringVar(&migrationsPath, "migrations-path", "migrations", "The path where the migrations stored.")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "The name of the migrations table.")
+	flag.Parse()
+
+	if storagePath == "" {
+		panic("storage-path is required")
+	}
+
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("nothing to migrate.")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied successfully.")
+}
--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module sso
 
-go 1.22.5
+go 1.23.0
+
+toolchain go1.24.1
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	golang.org/x/crypto v0.39.0
 	google.golang.org/grpc v1.68.1
@@ -12,10 +15,14 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
